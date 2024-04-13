49d1932292167c1e1451756c30811984712cf841
Hugh Osborne <hugh.osborne@gmail.com>
2024-04-13 22:50:44 +0100

    backup

--- untitled/untitled.py
+++ untitled/untitled.py
@@ -10,6 +10,18 @@ class Bone:
         # The location of the pivot point (origin) in the parent space is stored.
         self.origin_in_parent_space = np.zeros(3)
         self.rotation = np.zeros(3)
+        self.mass = 1.0
+        self.inertia = np.zeros(3)
+        self.rot_acc = np.zeros(3)
+        self.rot_vel = np.zeros(3)
+        
+    def update(self):
+        
+        self.inertia = (self.mass * (self.length**2))/3.0
+        torque = 9.81*self.length # torque = Fr
+        ang_acc = torque / self.inertia
+        
+        
         
 class Muscle:
     def __init__(self):
