6dcc0775a444caf06c35844342b279cf437159cf
Hugh Osborne <hugh.osborne@gmail.com>
2024-05-09 22:25:12 +0100

    working?

--- untitled/simulation.py
+++ untitled/simulation.py
@@ -23,15 +23,36 @@ O.set_vel(N, 0) # lock the origin by setting the vel to zero (required for v2pt_
 
 # object A
 m_a = sm.symbols('m_a') # mass
-a_q0,a_q1,a_q2,a_q3 = me.dynamicsymbols('a_q0, a_q1, a_q2, a_q3') # angle from the vertical in N <- This must be more general
+a_psi, a_theta, a_phi = me.dynamicsymbols('a_psi, a_theta, a_varphi') # angle from the vertical in N <- This must be more general
 a_u0,a_u1,a_u2 = me.dynamicsymbols('a_u0, a_u1, a_u2') # q1 prime
 
-q1 = (a_q0,a_q1,a_q2,a_q3)
-u1 = a_u0*N.x + a_u1*N.y + a_u2*N.z
-
+A1 = me.ReferenceFrame('A1')
+A2 = me.ReferenceFrame('A2')
+A3 = me.ReferenceFrame('A3')
 A = me.ReferenceFrame('A')
-A.orient_quaternion(N, q1) # A's reference frame is rotated through angle q1 around N.z <- This should be more general
-A.set_ang_vel(N, u1) # A's reference frame has angular velocity u1 around N.z
+#A.orient_quaternion(N, (1.0, a_q0,a_q1,a_q2)) # A's reference frame is rotated through angle q1 around N.z <- This should be more general
+A.orient_body_fixed(N, (a_psi, a_theta, a_phi), 'XYZ')
+#A1.orient_axis(N, N.y, a_psi)
+#A2.orient_axis(A1, A1.x, a_theta)
+#A3.orient_axis(A2, A2.z, a_phi)
+
+# Put the orientation and angular speed into matrices
+a_q = sm.Matrix([a_psi, a_theta, a_phi])
+a_u = sm.Matrix([a_u0, a_u1, a_u2])
+
+# Define the time differentials of orientation and angular speed
+a_qd = a_q.diff(t)
+a_ud = a_u.diff(t)
+
+N_w_A = A.ang_vel_in(N)
+N_w_A = N_w_A.xreplace(dict(zip(a_qd, a_u)))
+A.set_ang_vel(N, N_w_A)
+
+N_w_A = A.ang_acc_in(N)
+N_w_A = N_w_A.xreplace(dict(zip(a_qd, a_u)))
+ud_zerod = {udr: 0 for udr in a_ud}
+N_w_A = N_w_A.xreplace(ud_zerod)
+A.set_ang_acc(N, N_w_A)
 
 Ao = me.Point('A_O') # A centre of mass
 Ao.set_pos(O, 0.5*A.x) # Set the location of the com in relation to the origin
@@ -42,40 +63,21 @@ I_A_Ao = I*me.outer(A.y, A.y) + I*me.outer(A.z, A.z) # Inertia of A
 # Constraint?
 Ao.v2pt_theory(O, N, A) # The velocity of the com (in frame N) changes with A (with respect to O)
 
-# object B
-m_b = sm.symbols('m_b') 
-q2 = me.dynamicsymbols('q2') # angle from the horizontal in A
-u2  = me.dynamicsymbols('u2') # q2 prime
+# N_v_A = Ao.vel(N)
+# N_v_A = N_v_A.xreplace(dict(zip(a_qd, a_u)))
+#Ao.set_vel(N, N_v_A)
 
-b_q0,b_q1,b_q2,b_q3 = me.dynamicsymbols('b_q0, b_q1, b_q2, b_q3') # angle from the vertical in N <- This must be more general
-b_u0,b_u1,b_u2 = me.dynamicsymbols('b_u0, b_u1, b_u2') # q1 prime
+N_v_A = Ao.acc(N)
+N_v_A = N_v_A.xreplace(dict(zip(a_qd, a_u)))
 
-q2 = (b_q0,b_q1,b_q2,b_q3)
-u2 = b_u0*N.x + b_u1*N.y + b_u2*N.z
-
-B = me.ReferenceFrame('B')
-B.orient_quaternion(A, q2) # B's reference frame is rotated through angle q2 around A.x
-B.set_ang_vel(A, u2) # B's reference frame has angular velocity u2 around A.x
-
-Bo = me.Point('B_O') # B centre of mass
-Bo.set_pos(O, A.x) # Set the location of the com in relation to the origin
-print(Bo.pos_from(O).to_matrix(N))
-I = m_b**2/12
-I_B_Bo = I*me.outer(B.x, B.x) + I*me.outer(B.z, B.z) # Inertia of B 
-
-# Constraint?
-Bo.v2pt_theory(O, N, A) # The velocity of the com (in frame N) changes with A (with respect to O) <- this should be changes with B to be more general
+Ao.set_acc(N, N_v_A)
 
 # Force of gravity on A (applied at the com)
 R_Ao = m_a*g*N.x
 
-# Force of gravity on B (applied at the com)
-R_Bo = m_b*g*N.x
-
 # Spring Forces
 kt = sm.symbols('k_t')
 T_A = -kt*N.z
-T_B = -kt*A.x
 
 Fr_bar = [] # This defines the total "Partial forces" (Generalised Active Forces) for each object: the force component of each angular speed
 Frs_bar = [] # This defines the time-derivative solutions to the partial forces (Generalised Inertia Forces) for each object in terms of the mass, acceleration, inertia, etc.
@@ -84,26 +86,28 @@ Frs_bar = [] # This defines the time-derivative solutions to the partial forces 
 # Frs is the "right-hand side"
 # Fr = (u.R) + (w.T) = (-m*a.u) + (a.I + wxI.w) = Frs
 
+
+
 # Now we loop through each object and get its angular speed
-for ui in [u1, u2]:
+for ui in [a_u0, a_u1, a_u2]:
     Fr = 0
     Frs = 0
     # The angular speed of each object is dependent on the forces applied to all connected objects
     # First, deal with the active forces (that move the centre of mass without rotation)
-    for Pi, Ri, mi in zip([Ao, Bo], [R_Ao, R_Bo], [m_a, m_b]):
-        print(Pi.vel(N))
+    for Pi, Ri, mi in zip([Ao], [R_Ao], [m_a]):
         vr = Pi.vel(N).diff(ui, N)
         Fr += vr.dot(Ri)
         Rs = -mi*Pi.acc(N)
         Frs += vr.dot(Rs)
         
     # Now deal with the inertia forces (that caus rotations) - in this example, we have no forces but this is where muscle forces will be applied
-    for Bi, Ti, Ii in zip([A, B], [T_A, T_B], [I_A_Ao, I_B_Bo]):
+    for Bi, Ti, Ii in zip([A], [T_A], [I_A_Ao]):
         wr = Bi.ang_vel_in(N).diff(ui, N)
         Fr += wr.dot(Ti)
         Ts = -(Bi.ang_acc_in(N).dot(Ii) +
                me.cross(Bi.ang_vel_in(N), Ii).dot(Bi.ang_vel_in(N)))
         Frs += wr.dot(Ts)
+        
         
     Fr_bar.append(Fr)
     Frs_bar.append(Frs)
@@ -113,51 +117,48 @@ Fr = sm.Matrix(Fr_bar)
 Frs = sm.Matrix(Frs_bar)
 
 # Put the orientation and angular speed into matrices
-q = sm.Matrix([q1, q2])
-u = sm.Matrix([u1, u2])
+q = sm.Matrix([a_psi, a_theta, a_phi])
+u = sm.Matrix([a_u0, a_u1, a_u2])
 
 # Define the time differentials of orientation and angular speed
 qd = q.diff(t)
 ud = u.diff(t)
+
+print(ud)
 
 # Set the acceleration to 0
 ud_zerod = {udr: 0 for udr in ud}
 
 # This is the site of forward kinematics vs inverse kinematics
 # Initialise Mk (kinematics) - For forward kinematics, we know the current speeds
-Mk = -sm.eye(2)
+Mk = -sm.eye(3)
 gk = u
 
 # Initialise Md (dynamics) - For forward kinematics, we know the component forces and time derivatives 
 Md = Frs.jacobian(ud)
 gd = Frs.xreplace(ud_zerod) + Fr
 
-print(Mk, gk, Md, gd)
-
 # Now we define a function that evaluates the equations of motion (eom) - returning the calculated Mk, gk, Md, and gd
 # The function takes the current orientations or the objects, the current angular speeds, masses, g
-m =  sm.Matrix([m_a,m_b])
+m =  sm.Matrix([m_a])
 p =  sm.Matrix([g, kt])
 eval_eom = sm.lambdify((q, u, m, p), [Mk, gk, Md, gd])
 
 # get reference frames
-get_A = sm.lambdify((q1), A.dcm(N))
-get_B = sm.lambdify((q1,q2), B.dcm(N))
+# x,y,z = sm.symbols('x,y,z')
+# p_in_A = x*A.x + y*A.y + z*A.z
+# p_in_A = p_in_A.to_matrix(N)
+# get_A_pos_in_N = sm.lambdify((x,y,z,a_psi, a_theta, a_phi), p_in_A)
 
 # Initialise the inputs with starting values
-q_vals = np.array([
-    np.deg2rad(25.0),  # q1, rad
-    np.deg2rad(5.0),  # q2, rad
+q_vals = np.array([0.0,1.0,0.0  # q2, rad
 ])
 
-u_vals = np.array([
-    0.1,  # u1, rad/s
-    2.2,  # u2, rad/s
+u_vals = np.array([0.0,0.1,0.0
 ])
 
 m_vals = np.array([
-    1.0,  # m_a, kg
-    1.0,  # m_b, kg
+    1.0  # m_a, kg
 ])
 
 p_vals = np.array([
@@ -167,6 +168,11 @@ p_vals = np.array([
 
 # Now find the initial mass matrix components
 Mk_vals, gk_vals, Md_vals, gd_vals = eval_eom(q_vals, u_vals, m_vals, p_vals)
+
+print("Mk", Mk_vals)
+print("gk", gk_vals)
+print("Md", Md_vals)
+print("gd", gd_vals)
 
 # Now the hard work must be done: find the speeds and accelerations from the
 # system of equations defined by the mass matrix.
@@ -183,18 +189,16 @@ vis = Visualiser()
 vis.setupVisualiser()
 
 # initial state: q1, q2, u1,u2
-state = [np.deg2rad(25.0), np.deg2rad(5.0), 0.0, 0.0]
+state = [np.deg2rad(25.0), 0.0]
 
 for i in range(1000):
 
     q_vals = np.array([
-        state[0],  # q1, rad
-        state[1],  # q2, rad
+        0.0,0.0,state[0] # q2, rad
     ])
 
-    u_vals = np.array([
-        state[2],  # u1, rad/s
-        state[3],  # u2, rad/s
+    u_vals = np.array(
+        [0.0,0.0,state[1]  # u2, rad/s
     ])
     
     Mk_vals, gk_vals, Md_vals, gd_vals = eval_eom(q_vals, u_vals, m_vals, p_vals)
@@ -202,12 +206,11 @@ for i in range(1000):
     ud_vals = np.linalg.solve(-Md_vals, np.squeeze(gd_vals))
     
     state[0] += 0.01 * (qd_vals[0])
-    state[1] += 0.01 * (qd_vals[1])
-    state[2] += 0.01 * (ud_vals[0])
-    state[3] += 0.01 * (ud_vals[1])
+    state[1] += 0.01 * (ud_vals[0])
     
     vis.beginRendering()
-    vis.drawLine(np.array([0.0,0.0,0.0]), np.matmul(get_A(state[0]),np.array([0.0,-1.0,0.0])))
+    print(get_A_pos_in_N(0.0,-1.0,0.0,0.0,0.0,state[0]))
+    vis.drawLine(np.array([0.0,0.0,0.0]), np.matmul(get_A(0.0,0.0,state[0]),np.array([0.0,-1.0,0.0])))
     vis.drawCube(matrix=np.identity(4), model_pos=np.array([0.0,0.0,0.0]), scale=0.02, col=(1,0,0,1))
-    vis.drawCube(matrix=np.identity(4), model_pos=np.matmul(get_A(state[0]),np.array([0.0,-1.0,0.0])), scale=0.02, col=(1,0,0,1))
+    vis.drawCube(matrix=np.identity(4), model_pos=np.matmul(get_A(0.0,0.0,state[0]),np.array([0.0,-1.0,0.0])), scale=0.02, col=(1,0,0,1))
     vis.endRendering()
--- untitled/untitled.py
+++ untitled/untitled.py
@@ -24,29 +24,46 @@ class Object:
         self.O = self.parentOrigin
 
         self.mass = sm.symbols('m_' + name) # mass
-        
         self.frame = me.ReferenceFrame('F_' + name) # reference frame
-        self.r0 = me.dynamicsymbols('r0_' + name) # orientation quaternion
         self.r1 = me.dynamicsymbols('r1_' + name) # 
         self.r2 = me.dynamicsymbols('r2_' + name) # 
         self.r3 = me.dynamicsymbols('r3_' + name) # 
-        self.frame.orient_quaternion(self.N, (1.0,self.r1,self.r2,self.r3) ) # Reference frame is rotated
+        self.frame.orient_body_fixed(self.N, (self.r1, self.r2, self.r3), 'XYZ')
         self.u0 = me.dynamicsymbols('u0_' + name) # angular velocity
         self.u1 = me.dynamicsymbols('u1_' + name) # 
         self.u2 = me.dynamicsymbols('u2_' + name) #
         self.frame.set_ang_vel(self.N, self.u0*self.N.x + self.u1*self.N.y + self.u2*self.N.z) # Reference frame has angular velocity u
+         
+        self.com = me.Point('com_' + name)
+        self.q0 = sm.symbols('q0_' + name) # com location
+        self.q1 = sm.symbols('q1_' + name) # 
+        self.q2 = sm.symbols('q2_' + name) #
+        self.com.set_pos(self.O, self.q0*self.frame.x + self.q1*self.frame.y + self.q2*self.frame.z)
+        self.com.v2pt_theory(self.O, self.N, self.frame)
         
-        self.com = me.Point('com_' + name) # centre of mass
-        self.q0 = me.dynamicsymbols('q0_' + name) # com position in world coords
-        self.q1 = me.dynamicsymbols('q1_' + name) #
-        self.q2 = me.dynamicsymbols('q2_' + name) #
-        self.com.set_pos(self.O, self.q0*self.frame.x + self.q1*self.frame.y + self.q2*self.frame.z) 
-        self.v0 = me.dynamicsymbols('v0_' + name) # velocity
-        self.v1 = me.dynamicsymbols('v1_' + name) # 
-        self.v2 = me.dynamicsymbols('v2_' + name) # 
-        self.com.set_vel(self.N, self.v0*self.frame.x + self.v1*self.frame.y + self.v2*self.frame.z)
+        t = me.dynamicsymbols._t
+        # Put the orientation and angular speed into matrices
+        a_q = sm.Matrix([self.r1, self.r2, self.r3])
+        a_u = sm.Matrix([self.u0, self.u1, self.u2])
+
+        # Define the time differentials of orientation and angular speed
+        a_qd = a_q.diff(t)
+        a_ud = a_u.diff(t)
         
-        #self.com.v2pt_theory(self.O, self.N, self.frame)
+        N_w_A = self.frame.ang_vel_in(self.N)
+        N_w_A = N_w_A.xreplace(dict(zip(a_qd, a_u)))
+        self.frame.set_ang_vel(self.N, N_w_A)
+
+        N_w_A = self.frame.ang_acc_in(self.N)
+        N_w_A = N_w_A.xreplace(dict(zip(a_qd, a_u)))
+        ud_zerod = {udr: 0 for udr in a_ud}
+        N_w_A = N_w_A.xreplace(ud_zerod)
+        self.frame.set_ang_acc(self.N, N_w_A)
+        
+        N_v_A = self.com.acc(self.N)
+        N_v_A = N_v_A.xreplace(dict(zip(a_qd, a_u)))
+
+        self.com.set_acc(self.N, N_v_A)
 
         # Inertia matrix
         self.Ixx = sm.symbols('I_xx_' + name)
@@ -59,67 +76,47 @@ class Object:
         self.I = me.inertia(self.frame, self.Ixx, self.Iyy, self.Izz, self.Ixy, self.Iyz, self.Ixz)
         
         # symbol functions
-        self.get_A = sm.lambdify((self.r0, self.r1, self.r2, self.r3), self.frame.dcm(self.N))
+        self.get_A = sm.lambdify((self.r1, self.r2, self.r3), self.frame.dcm(self.N))
         
     def getFrFrsFromForce(self, force):
-        v_com_0 = self.com.vel(self.N).diff(self.v0, self.N, var_in_dcm=False)
-        v_com_1 = self.com.vel(self.N).diff(self.v1, self.N, var_in_dcm=False)
-        v_com_2 = self.com.vel(self.N).diff(self.v2, self.N, var_in_dcm=False)
-        v_com_3 = self.com.vel(self.N).diff(self.u0, self.N, var_in_dcm=False)
-        v_com_4 = self.com.vel(self.N).diff(self.u1, self.N, var_in_dcm=False)
-        v_com_5 = self.com.vel(self.N).diff(self.u2, self.N, var_in_dcm=False)
+        v_com_3 = self.com.vel(self.N).diff(self.u0, self.N)
+        v_com_4 = self.com.vel(self.N).diff(self.u1, self.N)
+        v_com_5 = self.com.vel(self.N).diff(self.u2, self.N)
         
-        F1 = v_com_0.dot(force)
-        F2 = v_com_1.dot(force)
-        F3 = v_com_2.dot(force)
         F4 = v_com_3.dot(force)
         F5 = v_com_4.dot(force)
         F6 = v_com_5.dot(force)
         
-        Fr = [F1, F2, F3, F4, F5, F6]
+        Fr = [F4, F5, F6]
         
         Rs = -self.mass*self.com.acc(self.N)
-        Ts = -(self.frame.ang_acc_in(self.N).dot(self.I) + me.cross(self.frame.ang_vel_in(self.N), self.I).dot(self.frame.ang_vel_in(self.N)))
 
-        F1s = v_com_0.dot(Rs)
-        F2s = v_com_1.dot(Rs)
-        F3s = v_com_2.dot(Rs)
         F4s = v_com_3.dot(Rs)
         F5s = v_com_4.dot(Rs)
         F6s = v_com_5.dot(Rs)
         
-        Frs = [F1s, F2s, F3s, F4s, F5s, F6s]
+        Frs = [F4s, F5s, F6s]
         
         return Fr, Frs
 
     def getFrFrsFromTorque(self, torque):
-        w_com_0 = self.frame.ang_vel_in(self.N).diff(self.v0, self.N, var_in_dcm=False)
-        w_com_1 = self.frame.ang_vel_in(self.N).diff(self.v1, self.N, var_in_dcm=False)
-        w_com_2 = self.frame.ang_vel_in(self.N).diff(self.v2, self.N, var_in_dcm=False)
-        w_com_3 = self.frame.ang_vel_in(self.N).diff(self.u0, self.N, var_in_dcm=False)
-        w_com_4 = self.frame.ang_vel_in(self.N).diff(self.u1, self.N, var_in_dcm=False)
-        w_com_5 = self.frame.ang_vel_in(self.N).diff(self.u2, self.N, var_in_dcm=False)
+        w_com_3 = self.frame.ang_vel_in(self.N).diff(self.u0, self.N)
+        w_com_4 = self.frame.ang_vel_in(self.N).diff(self.u1, self.N)
+        w_com_5 = self.frame.ang_vel_in(self.N).diff(self.u2, self.N)
         
-        F1 = w_com_0.dot(torque)
-        F2 = w_com_1.dot(torque)
-        F3 = w_com_2.dot(torque)
         F4 = w_com_3.dot(torque)
         F5 = w_com_4.dot(torque)
         F6 = w_com_5.dot(torque)
         
-        Fr = [F1, F2, F3, F4, F5, F6]
+        Fr = [F4, F5, F6]
         
-        Rs = -self.mass*self.com.acc(self.N)
         Ts = -(self.frame.ang_acc_in(self.N).dot(self.I) + me.cross(self.frame.ang_vel_in(self.N), self.I).dot(self.frame.ang_vel_in(self.N)))
 
-        F1s = w_com_0.dot(Ts)
-        F2s = w_com_1.dot(Ts)
-        F3s = w_com_2.dot(Ts)
         F4s = w_com_3.dot(Ts)
         F5s = w_com_4.dot(Ts)
         F6s = w_com_5.dot(Ts)
         
-        Frs = [F1s, F2s, F3s, F4s, F5s, F6s]
+        Frs = [F4s, F5s, F6s]
         
         return Fr, Frs
 
@@ -133,15 +130,13 @@ class Object:
         self.state_com = c
         
     def updateState(self, qd, ud, dt):
-        self.state_orientation += dt * qd[3:] # This will be r1,r2,r3
-        self.state_ang_vel += dt * ud[3:] # This will be u0,u1,u2
-        self.state_vel += dt * ud[:3] # This will be v0,v1,v2
-        self.state_com += dt * qd[:3] # This will be q0,q1,q2
+        self.state_orientation += dt * qd # This will be r1,r2,r3
+        self.state_ang_vel += dt * ud # This will be u0,u1,u2
         
     def draw(self, vis):
-        vis.drawLine(np.array([0.0,0.0,0.0]), np.matmul(self.get_A(1.0,self.state_orientation[0],self.state_orientation[1],self.state_orientation[2]),self.state_com))
+        vis.drawLine(np.array([0.0,0.0,0.0]), np.matmul(self.get_A(self.state_orientation[0],self.state_orientation[1],self.state_orientation[2]),self.state_com))
         vis.drawCube(matrix=np.identity(4), model_pos=np.array([0.0,0.0,0.0]), scale=0.02, col=(1,0,0,1))
-        vis.drawCube(matrix=np.identity(4), model_pos=np.matmul(self.get_A(1.0,self.state_orientation[0],self.state_orientation[1],self.state_orientation[2]),self.state_com), scale=0.02, col=(1,0,0,1))
+        vis.drawCube(matrix=np.identity(4), model_pos=np.matmul(self.get_A(self.state_orientation[0],self.state_orientation[1],self.state_orientation[2]),self.state_com), scale=0.02, col=(1,0,0,1))
 
     # @classmethod
     # def buildObjectFromInertiaMatrix(cls, I_xx, I_yy, I_zz, I_xy, I_xz, I_yz, referenceFrame=np.identity(3), com=np.zeros((3,1)), name="object", mass=1.0):
@@ -211,21 +206,21 @@ class Model:
 
         # Now we loop through each object and get its angular speed
         for name, obj in self.objects.items():
-            Fr = [0,0,0,0,0,0]
-            Frs = [0,0,0,0,0,0]
+            Fr = [0,0,0]
+            Frs = [0,0,0]
             # The angular speed of each object is dependent on the forces applied to all connected objects
             # First, deal with the active forces (that move the centre of mass without rotation)
             for fname, force_obj in self.objects.items(): # can this be reduced so we're not getting forces for all objects?
                 for force in force_obj.forces:
                     fr, frs = force_obj.getFrFrsFromForce(force)
-                    Fr = [Fr[i] + fr[i] for i in range(6)]
-                    Frs = [Frs[i] + frs[i] for i in range(6)]
+                    Fr = [Fr[i] + fr[i] for i in range(3)]
+                    Frs = [Frs[i] + frs[i] for i in range(3)]
             
             for tname, torque_obj in self.objects.items(): # can this be reduced so we're not getting forces for all objects?
                 for torque in torque_obj.torques:
                     fr, frs = torque_obj.getFrFrsFromTorque(torque)
-                    Fr = [Fr[i] + fr[i] for i in range(6)]
-                    Frs = [Frs[i] + frs[i] for i in range(6)]
+                    Fr = [Fr[i] + fr[i] for i in range(3)]
+                    Frs = [Frs[i] + frs[i] for i in range(3)]
         
             Fr_bar.append(Fr)
             Frs_bar.append(Frs)
@@ -236,20 +231,22 @@ class Model:
 
         # Put the orientation and angular speed into matrices
         Is = sm.Matrix([[self.objects[o].Ixx,self.objects[o].Iyy,self.objects[o].Izz,self.objects[o].Ixy,self.objects[o].Iyz,self.objects[o].Ixz] for o in self.objects])
-        qs = sm.Matrix([[self.objects[o].q0,self.objects[o].q1,self.objects[o].q2,self.objects[o].r1,self.objects[o].r2,self.objects[o].r3] for o in self.objects])
-        us = sm.Matrix([[self.objects[o].v0,self.objects[o].v1,self.objects[o].v2,self.objects[o].u0,self.objects[o].u1,self.objects[o].u2] for o in self.objects])
+        qs = sm.Matrix([[self.objects[o].r1,self.objects[o].r2,self.objects[o].r3] for o in self.objects])
+        us = sm.Matrix([[self.objects[o].u0,self.objects[o].u1,self.objects[o].u2] for o in self.objects])
         ms = sm.Matrix([self.objects[o].mass for o in self.objects])
+        cs = sm.Matrix([[self.objects[o].q0,self.objects[o].q1,self.objects[o].q2] for o in self.objects])
 
         # Define the time differentials of orientation and angular speed
         self.qd = qs.diff(self.t)
         self.ud = us.diff(self.t)
+        print(self.ud)
 
         # Set the acceleration to 0
         ud_zerod = {udr: 0 for udr in self.ud}
 
         # This is the site of forward kinematics vs inverse kinematics
         # Initialise Mk (kinematics) - For forward kinematics, we don't know the velocities but want to find them
-        self.Mk = -sm.eye(6)
+        self.Mk = -sm.eye(3)
         self.gk = us
 
         # Initialise Md (dynamics) - For forward kinematics, we know the component forces and time derivatives 
@@ -257,16 +254,18 @@ class Model:
         self.gd = Frs.xreplace(ud_zerod) + Fr
 
         # Now build the equations of motion function that will be applied each time step
-        self.eval_eom = sm.lambdify((qs, us, ms, Is), [self.Mk, self.gk, self.Md, self.gd])
+        self.eval_eom = sm.lambdify((cs, qs, us, ms, Is), [self.Mk, self.gk, self.Md, self.gd])
         
     def solve(self):
         q_vals = []
         u_vals = []
         m_vals = []
         i_vals = []
+        com_vals = []
         for name, obj in self.objects.items():
-            q_vals += [obj.state_com[0], obj.state_com[1], obj.state_com[2], obj.state_orientation[0], obj.state_orientation[1], obj.state_orientation[2]]
-            u_vals += [obj.state_vel[0], obj.state_vel[1], obj.state_vel[2], obj.state_ang_vel[0], obj.state_ang_vel[1], obj.state_ang_vel[2]]
+            com_vals += [obj.state_com[0], obj.state_com[1], obj.state_com[2]]
+            q_vals += [obj.state_orientation[0], obj.state_orientation[1], obj.state_orientation[2]]
+            u_vals += [obj.state_ang_vel[0], obj.state_ang_vel[1], obj.state_ang_vel[2]]
             m_vals += [obj.state_mass]
             i_vals += [obj.state_inertia[0,0], obj.state_inertia[1,1], obj.state_inertia[2,2], obj.state_inertia[0,1], obj.state_inertia[1,2], obj.state_inertia[0,2]]
             
@@ -274,8 +273,9 @@ class Model:
         u_vals = np.array(u_vals)
         m_vals = np.array(m_vals)
         i_vals = np.array(i_vals)
+        com_vals = np.array(com_vals)
         
-        Mk_vals, gk_vals, Md_vals, gd_vals = self.eval_eom(q_vals, u_vals, m_vals, i_vals)
+        Mk_vals, gk_vals, Md_vals, gd_vals = self.eval_eom(com_vals, q_vals, u_vals, m_vals, i_vals)
 
         # Now the hard work must be done: find the speeds and accelerations from the
         # system of equations defined by the mass matrix.
@@ -302,7 +302,7 @@ groundOrigin.set_vel(groundFrame, 0) # lock the origin by setting the vel to zer
 obj = Object(groundOrigin, groundFrame)
 obj.setStateCom(np.array([0.0,-0.5,0.0]))
 obj.setStateOrientation(np.array([0.0,0.0,0.3]))
-obj.addForce(obj.mass*9.81*groundFrame.x)
+obj.addForce(obj.mass*-9.81*groundFrame.y)
 obj.addTorque(0.0*groundFrame.z)
 mod = Model()
 
